6ca49ead030204bdca2fe128c8ae0a6f2343ce32
human-sketch <73455299+human-sketch@users.noreply.github.com>
2021-05-19 11:51:08 +1200

    Touched Up Joystick Controls

--- RocketLeague-2021-05-05T03-26-37/src/main.cpp
+++ RocketLeague-2021-05-05T03-26-37/src/main.cpp
@@ -90,15 +90,17 @@ void usercontrol(void) {
     
       if (Controller1.Axis3.position() - Controller1.Axis2.position() > 50)  {
         motorspeed = 90;
+        //if bot joystick position turning, limit speed
       }
       else if (Controller1.Axis2.position() - Controller1.Axis3.position() > 50) {
         motorspeed = 90;
+        //if bot joystick position turning, limit spee (opposite)
       }
       else {
-
-      }
         motor_left.spin(vex::directionType::fwd, Controller1.Axis3.position(vex::percentUnits::pct) * motorspeed, vex::velocityUnits::pct);
         motor_right.spin(vex::directionType::rev, Controller1.Axis2.position(vex::percentUnits::pct) * motorspeed, vex::velocityUnits::pct);
+      }
+        
 
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
